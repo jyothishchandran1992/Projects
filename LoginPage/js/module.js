8fcc25f19461a7246714a6cd3610b3429098fb29
jyothishchandran1992 <jyothishchandran1992@gmail.com>
2015-08-16 21:42:48 +0530

    Completed the Email-id validation
    Completed the Email-id validation

--- LoginPage/js/module.js
+++ LoginPage/js/module.js
@@ -1,11 +1,9 @@
-      var app = angular.module('LoginPage', []);
+var app = angular.module('LoginPage', []);
 app.controller('LoginPageController', function($scope) {
 	$scope.email = {
 		text : "",
-		valid : true,
-		errorrequiredempty : "Email Address is required",
-		errorrequiredminlength : "Your Email Address is too short",
-		errorrequiredvalid : "Please provide a valid Email Address",
-		class : "form-group has-error"
-		};
+	};
+	$scope.pwd = {};
+	
+	
 });
