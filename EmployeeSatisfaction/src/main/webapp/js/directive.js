f5bb9646fd002dd73129e742ebf18ac05ae827af
jyothishchandran1992 <jyothishchandran1992@gmail.com>
2015-09-20 10:17:51 +0530

    delete
    delete

--- EmployeeSatisfaction/src/main/webapp/js/directive.js
+++
@@ -1,7 +0,0 @@
-var app = angular.module('LoginPage', []);
-app.directive('ValidateForm', function() {
-  return {
-	restrict: 'E',
-    template: 'Name: {{customer.name}} Address: {{customer.address}}'
-  };
-});
